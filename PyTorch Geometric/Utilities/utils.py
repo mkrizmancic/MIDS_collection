deeb470571b726e478d6fb13c7c5c0a4fb072972
Marko Krizmancic <marko.krizmancic@fer.hr>
2023-05-18 16:50:52 +0200

    [PyG] Add proto check function

--- PyTorch Geometric/Utilities/utils.py
+++ PyTorch Geometric/Utilities/utils.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 import networkx as nx
 from matplotlib import pyplot as plt
 
@@ -11,7 +12,7 @@ def find_MIDS(G):
     Gc = nx.complement(G)
     min_size = len(Gc)
     min_cliques = []
-    for nodes in nx.find_cliques_recursive(Gc):
+    for nodes in nx.find_cliques(Gc):
         size = len(nodes)
         if size < min_size:
             min_size = size
@@ -19,6 +20,33 @@ def find_MIDS(G):
         elif size == min_size:
             min_cliques.append(nodes)
     return min_cliques
+
+
+def check_MIDS(A, candidate, target_value):
+    """
+    Args:
+        - A: adjacency matrix
+        - candidate: node labels that are candidate for MIDS (data.y)
+        - target_value: known size of the MIDS
+    """
+    # TODO: This function needs to be adjusted.
+    #   - Instead of adjacency matrix, we may pass the edgelist and convert it here
+
+    n = len(candidate)
+
+    # Candidate set is not minimal
+    if sum(candidate) > target_value:
+        return False
+    
+    # Candidate set is not dominating and independent
+    if not all((A + np.eye(n)) @ candidate == 1):
+        return False
+    
+    if sum(candidate) < target_value:
+        print("Somehow we found an even smaller MIDS.")
+    
+    return True
+
 
 def test_find_MIDS():
     graph_file = '/home/marko/PROJECTS/MIDS_collection/PyTorch Geometric/Dataset/raw/5_nodes/G5,5.txt'
